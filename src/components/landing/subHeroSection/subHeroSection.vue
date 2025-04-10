8d5ae61b8f335d737c38f46928ca2f7c8997d9ed
Choyan Barua <choyanbaruakuetcse@gmail.com>
2025-04-11 02:50:36 +0600

    fixed subhero

--- src/components/landing/subHeroSection/subHeroSection.vue
+++ src/components/landing/subHeroSection/subHeroSection.vue
@@ -3,17 +3,17 @@
         class="w-full border-y bg-bluish/50 border-lightGrey text-lightGrey flex justify-around uppercase lg:text-4xl md:text-2xl sm:text-4xl py-6 md:flex-row sm:flex-col items-center gap-4">
         <div class="flex">
             <CaExplore class="md:block sm:hidden" />
-            <p class="md:block sm:hidden">
+            <p class="md:block sm:hidden text-center">
                 Explorer
             </p>
         </div>
         <div class="flex">
             <CaFavoriteHalf class="md:block sm:hidden" />
-            <p class="md:block sm:hidden">Passionate</p>
+            <p class="md:block sm:hidden text-center">Passionate</p>
         </div>
         <div class="flex">
             <BxCodeAlt />
-            <p>Problem Solver</p>
+            <p class="text-center">Problem Solver</p>
         </div>
 
     </div>
